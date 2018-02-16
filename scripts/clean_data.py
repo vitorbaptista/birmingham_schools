52fdabb40b42bf24290f51439a8e0a666da2ab4b
Vitor Baptista <vitor@vitorbaptista.com>
2018-02-16 17:20:16 +0000

    Lowercase all headers and fix Kingsland URL

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import tempfile
 
 path = os.path.join(
@@ -6,14 +7,27 @@ path = os.path.join(
     '../data/birmingham_schools.csv'
 )
 
+def clean_data(path):
+    with open(path) as inp, tempfile.NamedTemporaryFile('w', delete=False) as output:
+        # Lowercase all headers
+        headers = inp.readline().strip().lower().split(',')
 
-with open(path, 'rb') as inp, tempfile.NamedTemporaryFile(delete=False) as output:
-    for row in inp:
-        modified_row = row.replace(
-            b'www.kingsland.bham.sch.uk/',
-            b'http://www.kingsland.bham.sch.uk/'
-        )
-        output.write(modified_row)
+        reader = csv.DictReader(inp, fieldnames=headers)
+        writer = csv.DictWriter(output, fieldnames=headers)
+        writer.writeheader()
+
+        for row in reader:
+            row = fix_kingsland_url(row)
+            writer.writerow(row)
+
+    os.replace(output.name, path)
 
 
-os.replace(output.name, path)
+def fix_kingsland_url(row):
+    if row['web_site'] == 'www.kingsland.bham.sch.uk/':
+        row['web_site'] = 'http://{}'.format(row['web_site'])
+    return row
+
+
+if __name__ == '__main__':
+    clean_data(path)
